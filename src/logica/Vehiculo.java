64b74534ffa1ed27c8054aaeca9126e4e23bdf44
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 17:51:16 -0500

    Add vehicle fields to Vehiculo class
    Se agregaron los atributos de tipo privado para la Clase Vehiculo.

--- src/logica/Vehiculo.java
+++ src/logica/Vehiculo.java
@@ -1,5 +1,11 @@
 package logica;
 
 public class Vehiculo {
+	
+	private String VINString;
+	private String marcaString;
+	private int NumPasajeros;
+	private String propietario;
+	private int año;
 
 }
