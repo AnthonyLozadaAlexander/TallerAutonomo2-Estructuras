3978f02d5f342984babc0d22047975b3cc6b459d
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-25 15:44:04 -0500

    Update - Clases
    Se agregaron cambios en las clases, se implemento los atributos modelo y precio en la Class Vehiculo, se implemento el TADVector y Comentarios para explicar el codigo de algunas clases

--- src/main/Escritura.java
+++ src/main/Escritura.java
@@ -9,22 +9,26 @@ public class Escritura <T>{
 	private FileOutputStream archivo;
 	private ObjectOutputStream escritura;
 	
+	// constructor para inicializar el nombre del archivo
 	public Escritura(String nombreArchivo) {
 		super();
 		this.nombreArchivo = nombreArchivo;
 	}
 	
+	// metodo para abrir el archivo y crear el flujo de escritura
 	void abrir() throws IOException {
 		archivo = new FileOutputStream(nombreArchivo);
 		escritura = new ObjectOutputStream(archivo);
 	}
 	
+	// metodo para escribir un dato en el archivo
 	void escribir(T dato) throws IOException {
 		if(escritura != null) { // si el flujo de escritura no es null
 			escritura.writeObject(dato);
 		}
 	}
 	
+	// metodo para cerrar el flujo de escritura y el archivo
 	void cerrar() throws IOException {
 		if(escritura != null) { // si el flujo de escritura no es null
 			escritura.close();
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 package main;
 
 public class Main {
-	
 	// Interfaz Consola - Brian
 	public static void main(String[] args) {
 		System.out.println("hola");
