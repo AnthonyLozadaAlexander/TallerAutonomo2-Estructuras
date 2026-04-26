15c49dec7e63efb497780742138277bc619678d8
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-26 16:14:17 -0500

    Update - Main
    Se agregaron validaciones para capturar errores de entrada de datos y se ajustaron los input

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,97 @@ package main;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+	
+	static void alertaInt() {
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println("Debe Ingresar Un Entero Positivo, Por Favor Intente Nuevamente");
+		System.out.print("-> ");
+		System.out.println("----------------------------------------------------------------------");
+	}
+	
+	static void alertaDouble() {
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println("Debe Ingresar Un Numero Decimal Positivo, Por Favor Intente Nuevamente");
+		System.out.print("-> ");
+		System.out.println("----------------------------------------------------------------------");
+	}
+	
+	static double validarDouble(Scanner input) {
+		double n = 0.0;
+		boolean esValido = false;
+		while(!esValido) {
+			try {
+				if(!input.hasNextDouble()) {
+					System.out.println("Error: Formato Invalido, Se Espera Un Entero Positivo");
+					alertaInt();
+					input.nextLine(); // Limpiar toda la línea
+					esValido = false;
+				}else {
+					n = input.nextDouble();
+					if(n <= 0) {
+						System.out.println("Error: El Tamaño Del Vector Debe Ser Un Entero Positivo");
+						alertaDouble();
+						input.nextLine(); // Limpiar toda la línea
+						esValido = false;
+					}else {
+						System.out.println("Ingreso Valido");
+						input.nextLine(); // Limpiar el buffer de entrada
+						esValido = true;
+					}
+				}
+				
+			}catch(NumberFormatException e) {
+				System.out.println("Error: Formato Invalido, Se Espera Un Entero Positivo");
+				input.nextLine(); // Limpiar toda la línea
+				esValido = false;
+		}
+		}
+		
+		return n;
+	}
+	static int validarEnteros(Scanner input) {
+		int n = 0;
+		boolean esValido = false;
+		while(!esValido) {
+			try {
+				if(!input.hasNextInt()) {
+					System.out.println("Error: Formato Invalido, Se Espera Un Entero Positivo");
+					alertaInt();
+					input.nextLine(); // Limpiar toda la línea
+					esValido = false;
+				}else {
+					n = input.nextInt();
+					if(n <= 0) {
+						System.out.println("Error: El Tamaño Del Vector Debe Ser Un Entero Positivo");
+						alertaInt();
+						input.nextLine(); // Limpiar toda la línea
+						esValido = false;
+					}else {
+						System.out.println("Ingreso Valido");
+						input.nextLine(); // Limpiar el buffer de entrada
+						esValido = true;
+					}
+				}
+				
+			}catch(NumberFormatException e) {
+				System.out.println("Error: Formato Invalido, Se Espera Un Entero Positivo");
+				input.nextLine(); // Limpiar toda la línea
+				esValido = false;
+		}
+		}
+		
+		return n;
+	}
+	
+	
 	// Interfaz Consola - Brian
 	public static void main(String[] args) {
-		ArrayList<Vehiculo> listaVehiculos = new ArrayList<Vehiculo>(); // ArrayList para almacenar los vehiculos              																		   registrados
+		ArrayList<Vehiculo> listaVehiculos = new ArrayList<Vehiculo>(); // ArrayList para almacenar los vehiculos														
 		Escritura<Vehiculo> escritor = new Escritura<>("Vehiculos.dat");
 		Lectura<Vehiculo> lector = new Lectura<Vehiculo>("Vehiculos.dat");
 		Scanner input = new Scanner(System.in).useLocale(Locale.US);
@@ -20,30 +104,25 @@ public class Main {
 		Vehiculo v;
 
 		do {
-			System.out.println("----------------------------------------------");
-			System.out.println("Bienvenido Al Sistema De Registro De Vehiculos");
-			System.out.println("----------------------------------------------");
-			System.out.println("1. Ingrese El Numero De Vehiculos A Registrar");
-			System.out.println("2. Generar Reporte De Vehiculos Registrados");
-			System.out.println("3. Salir");
-			System.out.println("---------------------------------------------");
+			System.out.println("--------------------------------------------------------------------");
+			System.out.println("           Bienvenido Al Sistema De Registro De Vehiculos");
+			System.out.println("--------------------------------------------------------------------");
+			System.out.println("           1. Ingrese El Numero De Vehiculos A Registrar");
+			System.out.println("           2. Generar Reporte De Vehiculos Registrados");
+			System.out.println("           3. Salir");
+			System.out.println("--------------------------------------------------------------------");
 			opc = input.nextLine();
 
 			switch (opc) {
 			case "1":
 				do {
-				// Ingreso Del Numero De Vehiculos A Registrar
-				System.out.println("______________________________________________");
-				System.out.println(" Ingrese El Numero De Vehiculos A Registrar: ");
-				System.out.println("______________________________________________");
-				if (!input.hasNextInt()) {
-					throw new NumberFormatException("Error: Formato Invalido, Se Espera Un Entero Positivo");
-				}
-				n = input.nextInt();
-				if (n <= 0) {
-					System.out.println("Error: El Tamaño Del Vector Debe Ser Un Entero Positivo");
-				}
-				}while(n <= 0);
+					// Ingreso Del Numero De Vehiculos A Registrar
+					System.out.println("______________________________________________");
+					System.out.println(" Ingrese El Numero De Vehiculos A Registrar: ");
+					System.out.println("______________________________________________");
+					n = validarEnteros(input);
+
+				} while (n <= 0);
 
 				input.nextLine(); // Limpiar el buffer de entrada
 
@@ -82,38 +161,19 @@ public class Main {
 						modelo = input.nextLine();
 						System.out.println("______________________________________________");
 						System.out.println("Ingrese El Numero De Pasajeros Del Vehiculo:");
-						numPasajeros = input.nextInt();
-						input.nextLine(); // Limpiar el buffer
-						do {
-							esValido = true;
-							System.out.println("______________________________________________");
-							System.out.println("Ingrese El Precio Del Vehiculo:");
-							try {
-								if (!input.hasNextDouble()) {
-									System.out.println("Error: Formato Invalido");
-									input.nextLine(); // Limpiar toda la línea
-									esValido = false;
-									continue;
-								}
-								precio = input.nextDouble();
-								input.nextLine(); // Limpiar el buffer
-								System.out.println("______________________________________________");
-								System.out.println("Ingrese El Año Del Vehiculo:");
-								if (!input.hasNextInt()) {
-									System.out.println("Error: Formato Invalido");
-									input.nextLine(); // Limpiar toda la línea
-									esValido = false;
-									continue;
-								}
-								año = input.nextInt();
-								esValido = true;
-							} catch (Exception e) {
-								System.out.println("Error: Formato Invalido");
-								input.nextLine(); // Limpiar toda la línea
-								esValido = false;
-							}
-						} while (esValido == false);
-						input.nextLine(); // Limpiar el buffer
+						numPasajeros = validarEnteros(input);
+						// input.nextLine(); // Limpiar el buffer
+
+						System.out.println("______________________________________________");
+						System.out.println("Ingrese El Precio Del Vehiculo:");
+
+						precio = validarDouble(input);
+						// input.nextLine(); // Limpiar el buffer
+						System.out.println("______________________________________________");
+						System.out.println("Ingrese El Año Del Vehiculo:");
+						año = validarEnteros(input);
+						// input.nextLine(); // Limpiar el buffer
+
 						System.out.println("______________________________________________");
 
 					} while (numPasajeros <= 0 || precio <= 0 || año <= 0 || numPasajeros <= 0 || precio <= 0
@@ -142,36 +202,35 @@ public class Main {
 					}
 					// Leer contactos del archivo para verificar que se guardaron correctamente
 					try {
-						lector.abrir(); 
+						lector.abrir();
 						do {
 							v = lector.leer();
-							if(v != null) { // si v no es null, agregarlo a lista de Vehiculos
+							if (v != null) { // si v no es null, agregarlo a lista de Vehiculos
 								listaVehiculos.add(v);
 							}
-						}while(v != null);
+						} while (v != null);
 						lector.cerrar();
-					}catch(IOException e) {
+					} catch (IOException e) {
 						System.out.println("Error: No se pudo leer el archivo de vehiculos.");
-					}catch(ClassNotFoundException e) {
+					} catch (ClassNotFoundException e) {
 						System.out.println("Error: El formato del archivo de vehiculos es incorrecto.");
 					}
 
 				}
-
 
 				break;
 			case "2":
 				System.out.println("Generando Reporte De Vehiculos Registrados");
 				System.out.println("______________________________________________");
 				StringBuilder reporte = new StringBuilder();
-				for(Vehiculo veh: listaVehiculos) {
+				for (Vehiculo veh : listaVehiculos) {
 					reporte.append(veh.toString()).append("\n");
 				}
 				System.out.println("----------------------------------------------");
 				System.out.println("       Reporte De Vehiculos Registrados");
 				System.out.println("----------------------------------------------");
 				System.out.println(reporte.toString());
-				
+
 				break;
 			case "3":
 				System.out.println("Saliendo Del Sistema...");
